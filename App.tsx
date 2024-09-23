47b06c9fd69b0ba8e7d2842465eee42976d6a416
Venkata Naidu <Chowdaryrk077@gmail.com>
2024-09-23 12:17:05 -0400

    .env

--- App.tsx
+++ App.tsx
@@ -17,13 +17,10 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import Config from 'react-native-config';
+
+console.log(Config.API_URL);
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -76,20 +73,10 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Env">
+            <Text style={styles.highlight}>API URL: {Config.API_URL}</Text>
+            <Text style={styles.highlight}>API URL: {Config.ENV}</Text>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
